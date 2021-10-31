d3701f13692b3c57bae59e27f4c3a7be108953e7
Agung Kurniawan Syahferdi <agungsyahferdi69@gmail.com>
2021-10-31 21:41:32 +0700

    configure view guru

--- template/view_guru.php
+++ template/view_guru.php
@@ -1,3 +1,24 @@
+<?php
+$DB = DB::getInstance();
+
+
+$email_user = $_SESSION['email'];
+$guru = $DB->getWhereOnce('guru', ['email','=',$email_user]);
+$kelas = $DB->getWhereOnce('kelas', ['nig','=',$guru->nig]);
+
+$DB->select('siswa.*');
+$tabelSiswa = $DB->get('kelas_siswa', 'INNER JOIN siswa ON siswa.nis = kelas_siswa.nis WHERE idk=?', [$kelas->idk]);
+
+if (!empty($_GET)) {
+    $tableTemp = [];
+    foreach ($tabelSiswa as $siswa) {
+        if (preg_match("/". Input::get('search') ."/i", $siswa->nama)) {
+            $tabelTemp[] = $siswa;
+        }
+    }
+    $tabelSiswa = $tabelTemp;
+}
+?>
 <div class="container">
     <div class="row">
         <div class="col-12">
@@ -34,78 +55,18 @@
                             echo "<td>{$siswa->nama}</td>";
                             echo "<td>{$siswa->email}</td>";
                             echo "<td>{$siswa->alamat}</td>";
-                            if ($kategori_user == 'guru') {
-                                echo "<td class=\"text-center\">";
-                                echo "<a href=\"edit_user.php?id={$siswa->nis}\" class=\"btn btn-info\" disabled>Edit</a> ";
-                                echo "<a href=\"hapus_user.php?id={$siswa->nis}\" class=\"btn btn-danger\">Hapus</a>";
-                                echo "</td>";
-                            } elseif ($siswa->email == $_SESSION['email']) {
-                                echo "<td class='text-danger'><b>Sedang Login<b></td>";
-                            } else {
-                                echo "<td class=\"text-center\">-</td>";
-                            }
+                            echo "<td class=\"text-center\">";
+                            echo "<a href=\"edit_user.php?id={$siswa->nis}\" class=\"btn btn-info\" disabled>Edit</a> ";
+                            echo "<a href=\"hapus_user.php?id={$siswa->nis}\" class=\"btn btn-danger\">Hapus</a>";
+                            echo "</td>";
                             echo "</tr>";
                         }
                      ?>
                 </tbody>
             </table>
             <?php endif; ?>
-        </div>
-        <?php if ($kategori_user != 'siswa'): ?>
-        <div class="col-12">
-            <div class="py-4 d-flex justify-content-end align-items-center">
-                <h1 class="h2 mr-auto text-info">
-                    Daftar guru :
-                </h1>
+            <p><b>Kelas : (<?= $kelas->nama; ?>)</b></p>
 
-                <form class="w-25 ml-4" method="get">
-                    <div class="input-group">
-                        <input type="text" name="search" class="form-control" placeholder="Cari guru">
-                        <div class="input-group-append">
-                            <input type="submit" value="Cari" class="btn btn-outline-secondary">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <?php if (!empty($tabelGuru)) : ?>
-            <table class="table table-striped">
-                <thead>
-                    <tr class="text-center">
-                        <th>NIG</th>
-                        <th>Nama Siswa</th>
-                        <th>Email</th>
-                        <th>Alamat</th>
-                        <th>Action</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach ($tabelGuru as $guru) {
-                            echo "<tr class=\"text-center\">";
-                            echo "<th>{$guru->nig}</th>";
-                            echo "<td>{$guru->nama}</td>";
-                            echo "<td>{$guru->email}</td>";
-                            echo "<td>{$guru->alamat}</td>";
-                            if ($kategori_user == 'guru') {
-                                if ($guru->email == $_SESSION['email']) {
-                                    echo "<td class='text-danger'><b>Sedang Login<b></td>";
-                                } else {
-                                    echo "<td class=\"text-center\">";
-                                    echo "<a href=\"edit_user.php?id={$guru->nig}\" class=\"btn btn-info\" disabled>Edit</a> ";
-                                    echo "<a href=\"hapus_user.php?id={$guru->nig}\" class=\"btn btn-danger\">Hapus</a>";
-                                    echo "</td>";
-                                }
-                            } else {
-                                echo "<td class=\"text-center\">-</td>";
-                            }
-                            echo "</tr>";
-                        }
-                     ?>
-                </tbody>
-            </table>
-            <?php endif; ?>
         </div>
-        <?php endif; ?>
     </div>
 </div>
