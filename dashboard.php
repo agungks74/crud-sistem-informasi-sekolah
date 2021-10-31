5d18f7b161ef4b3688a054a1d89a0c46dda867bc
Agung Kurniawan Syahferdi <agungsyahferdi69@gmail.com>
2021-10-31 20:45:09 +0700

    memisah view guru admin dan siswa

--- dashboard.php
+++ dashboard.php
@@ -19,117 +19,17 @@ if (!empty($_GET)) {
 include "template/header.php";
 ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="py-4 d-flex justify-content-end align-items-center">
-                <h1 class="h2 mr-auto text-info">
-                    Daftar siswa :
-                </h1>
+<?php
+if ($kategori_user == 'siswa') {
+    include 'template/view_siswa.php';
+} elseif ($kategori_user == 'guru') {
+    include 'template/view_guru.php';
+} elseif ($kategori_user == 'admin') {
+    include 'template/view_admin.php';
+}
 
-                <form class="w-25 ml-4" method="get">
-                    <div class="input-group">
-                        <input type="text" name="search" class="form-control" placeholder="Cari siswa">
-                        <div class="input-group-append">
-                            <input type="submit" value="Cari" class="btn btn-outline-secondary">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <?php if (!empty($tabelSiswa)) : ?>
-            <table class="table table-striped">
-                <thead>
-                    <tr class="text-center">
-                        <th>NIS</th>
-                        <th>Nama Siswa</th>
-                        <th>Email</th>
-                        <th>Alamat</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach ($tabelSiswa as $siswa) {
-                            echo "<tr >";
-                            echo "<th>{$siswa->nis}</th>";
-                            echo "<td>{$siswa->nama}</td>";
-                            echo "<td>{$siswa->email}</td>";
-                            echo "<td>{$siswa->alamat}</td>";
-                            if ($kategori_user == 'guru') {
-                                echo "<td class=\"text-center\">";
-                                echo "<a href=\"edit_user.php?id={$siswa->nis}\" class=\"btn btn-info\" disabled>Edit</a> ";
-                                echo "<a href=\"hapus_user.php?id={$siswa->nis}\" class=\"btn btn-danger\">Hapus</a>";
-                                echo "</td>";
-                            } elseif ($siswa->email == $_SESSION['email']) {
-                                echo "<td class='text-danger'><b>Sedang Login<b></td>";
-                            } else {
-                                echo "<td class=\"text-center\">-</td>";
-                            }
-                            echo "</tr>";
-                        }
-                     ?>
-                </tbody>
-            </table>
-            <?php endif; ?>
-        </div>
-        <?php if ($kategori_user != 'siswa'): ?>
-        <div class="col-12">
-            <div class="py-4 d-flex justify-content-end align-items-center">
-                <h1 class="h2 mr-auto text-info">
-                    Daftar guru :
-                </h1>
+?>
 
-                <form class="w-25 ml-4" method="get">
-                    <div class="input-group">
-                        <input type="text" name="search" class="form-control" placeholder="Cari guru">
-                        <div class="input-group-append">
-                            <input type="submit" value="Cari" class="btn btn-outline-secondary">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <?php if (!empty($tabelGuru)) : ?>
-            <table class="table table-striped">
-                <thead>
-                    <tr class="text-center">
-                        <th>NIG</th>
-                        <th>Nama Siswa</th>
-                        <th>Email</th>
-                        <th>Alamat</th>
-                        <th>Action</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach ($tabelGuru as $guru) {
-                            echo "<tr class=\"text-center\">";
-                            echo "<th>{$guru->nig}</th>";
-                            echo "<td>{$guru->nama}</td>";
-                            echo "<td>{$guru->email}</td>";
-                            echo "<td>{$guru->alamat}</td>";
-                            if ($kategori_user == 'guru') {
-                                if ($guru->email == $_SESSION['email']) {
-                                    echo "<td class='text-danger'><b>Sedang Login<b></td>";
-                                } else {
-                                    echo "<td class=\"text-center\">";
-                                    echo "<a href=\"edit_user.php?id={$guru->nig}\" class=\"btn btn-info\" disabled>Edit</a> ";
-                                    echo "<a href=\"hapus_user.php?id={$guru->nig}\" class=\"btn btn-danger\">Hapus</a>";
-                                    echo "</td>";
-                                }
-                            } else {
-                                echo "<td class=\"text-center\">-</td>";
-                            }
-                            echo "</tr>";
-                        }
-                     ?>
-                </tbody>
-            </table>
-            <?php endif; ?>
-        </div>
-        <?php endif; ?>
-    </div>
-</div>
 
 <?php
 include "template/footer.php";
---
+++ template/view_siswa.php
@@ -0,0 +1,67 @@
+<?php
+$DB = DB::getInstance();
+
+$DB->select('kelas_siswa.idk');
+
+$email_user = $_SESSION['email'];
+$id_kelas = $DB->get('kelas_siswa', 'INNER JOIN siswa ON siswa.nis = kelas_siswa.nis WHERE email=?;', [$email_user])[0]->idk;
+
+$DB->select('kelas.nama AS nama_kelas, guru.nama AS wali_kelas');
+$kelas = $DB->get('kelas', 'INNER JOIN guru ON kelas.nig = guru.nig WHERE idk=?', [$id_kelas])[0];
+
+if (!empty($_GET)) {
+    $tabelSiswa = $DB->getLike("siswa", "nama_barang", "%". Input::get("search") ."%");
+} else {
+    $DB->select('siswa.*');
+    $tabelSiswa = $DB->get('kelas_siswa', 'INNER JOIN siswa ON siswa.nis = kelas_siswa.nis WHERE idk=?', [$id_kelas]);
+}
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="py-4 d-flex justify-content-end align-items-center">
+                <h1 class="h2 mr-auto text-info">
+                    Kelas <?= $kelas->nama_kelas; ?> :
+                </h1>
+
+
+
+                <form class="w-25 ml-4" method="get">
+                    <div class="input-group">
+                        <input type="text" name="search" class="form-control" placeholder="Cari siswa">
+                        <div class="input-group-append">
+                            <input type="submit" value="Cari" class="btn btn-outline-secondary">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <?php if (!empty($tabelSiswa)) : ?>
+            <table class="table table-striped">
+                <thead>
+                    <tr class="text-center">
+                        <th>NIS</th>
+                        <th>Nama Siswa</th>
+                        <th>Email</th>
+                        <th>Alamat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($tabelSiswa as $siswa) {
+                            echo "<tr >";
+                            echo "<th>{$siswa->nis}</th>";
+                            echo "<td>{$siswa->nama}</td>";
+                            echo "<td>{$siswa->email}</td>";
+                            echo "<td>{$siswa->alamat}</td>";
+                            echo "</tr>";
+                        }
+                     ?>
+                </tbody>
+            </table>
+            <?php endif; ?>
+            <br>
+            <p><b>Wali kelas : (<?= $kelas->wali_kelas; ?>)</b></p>
+        </div>
+    </div>
+</div>
